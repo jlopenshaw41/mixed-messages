ee5f43d264c57afeac33bfddd60ac8d689ab67cc
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2020-11-18 15:53:02 +0000

    completed project

--- main.js
+++ main.js
@@ -6,11 +6,17 @@ generate a random number (between 0 and array length)
 return that array element /console.log it
 */
 
-let quotes = ["1","2","3","4","5","6","7","8"];
+let forecasts = {
+    beginning: ["Today", "Tomorrow", "Next week", "This afternoon", "This weekend", "Tomorrow morning", "Tomorrow afternoon", "On Tuesday", "On Thursday"],
+    middle: ["it will be", "it is likely to be", "it looks like it will be", "it will probably be", "it will certainly be", "it is unlikely to be", "it definitely will not be"],
+    end: ["rainy","sunny","blustery","cloudy","stormy","mainly sunny","windy","hot", "cold"],
+} 
 
 const generateQuote = () => {
-    let randomNum = Math.floor(Math.random() * quotes.length);
-    return quotes[randomNum];
+    let randomNum1 = Math.floor(Math.random() * forecasts.beginning.length);
+    let randomNum2 = Math.floor(Math.random() * forecasts.middle.length);
+    let randomNum3 = Math.floor(Math.random() * forecasts.end.length);
+    return `${forecasts.beginning[randomNum1]} ${forecasts.middle[randomNum2]} ${forecasts.end[randomNum3]}.`;
 }
 
 console.log(generateQuote());
