3c8bd66e36b9d4e14a89d7532c3f6ff648268b41
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2020-12-04 15:29:01 +0000

    formatted document

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 let forecasts = {
-    beginning: ["Today", "Tomorrow", "Next week", "This afternoon", "This weekend", "Tomorrow morning", "Tomorrow afternoon", "On Tuesday", "On Thursday"],
-    middle: ["it will be", "it is likely to be", "it looks like it will be", "it will probably be", "it will certainly be", "it is unlikely to be", "it definitely will not be"],
-    end: ["rainy","sunny","blustery","cloudy","stormy","mainly sunny","windy","hot", "cold"],
-} 
+  beginning: [
+    "Today",
+    "Tomorrow",
+    "Next week",
+    "This afternoon",
+    "This weekend",
+    "Tomorrow morning",
+    "Tomorrow afternoon",
+    "On Tuesday",
+    "On Thursday",
+  ],
+  middle: [
+    "it will be",
+    "it is likely to be",
+    "it looks like it will be",
+    "it will probably be",
+    "it will certainly be",
+    "it is unlikely to be",
+    "it definitely will not be",
+  ],
+  end: [
+    "rainy",
+    "sunny",
+    "blustery",
+    "cloudy",
+    "stormy",
+    "mainly sunny",
+    "windy",
+    "hot",
+    "cold",
+  ],
+};
 
 const generateQuote = () => {
-    let randomNum1 = Math.floor(Math.random() * forecasts.beginning.length);
-    let randomNum2 = Math.floor(Math.random() * forecasts.middle.length);
-    let randomNum3 = Math.floor(Math.random() * forecasts.end.length);
-    return `${forecasts.beginning[randomNum1]} ${forecasts.middle[randomNum2]} ${forecasts.end[randomNum3]}.`;
-}
+  let randomNum1 = Math.floor(Math.random() * forecasts.beginning.length);
+  let randomNum2 = Math.floor(Math.random() * forecasts.middle.length);
+  let randomNum3 = Math.floor(Math.random() * forecasts.end.length);
+  return `${forecasts.beginning[randomNum1]} ${forecasts.middle[randomNum2]} ${forecasts.end[randomNum3]}.`;
+};
 
 console.log(generateQuote());
